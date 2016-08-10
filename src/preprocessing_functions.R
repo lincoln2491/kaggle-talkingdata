c771c0575b2a25564adbcb156816151cd3ec483b
lincoln2491 <lincoln2491@gmail.com>
2016-08-11 00:05:25 +0200

    geostatistics added

--- src/preprocessing_functions.R
+++ src/preprocessing_functions.R
@@ -1,6 +1,6 @@
 library(tidyr)
 library(reshape2)
-
+library(dplyr)
 
 split_timestamp <- function(data){
 	data = separate(data, col = timestamp, into = c("date", "time"), sep = " ")
@@ -9,72 +9,6 @@ split_timestamp <- function(data){
 	data$minutes = as.numeric(data$minutes)
 	data$seconds = as.numeric(data$seconds)
 	return(data)
-}
-
-load_data <- function(is_train = TRUE){
-	if(is_train){
-		gender_age = fread("data/gender_age_train.csv")
-	}
-	else{
-		gender_age = fread("data/gender_age_test.csv")
-	}
-	phone_brand_device_model = fread("data/phone_brand_device_model.csv")
-	gender_age_devices = merge(x = gender_age, y = phone_brand_device_model, 
-							   by = "device_id", all.x = TRUE)
-	print("gender_age_devices created")
-	rm(gender_age)
-	rm(phone_brand_device_model)
-	gc()
-	
-	events = fread("data/events.csv")
-	ids = unique(gender_age_devices$device_id)
-	events = events[device_id %in% ids]
-	gender_events = merge(x = gender_age_devices, y = events, by = "device_id", all.x = TRUE)
-	print("gender_events created")
-	rm(ids)
-	rm(events)
-	rm(gender_age_devices)
-	gc()
-	
-	app_events = fread("data/app_events.csv")
-	ids = unique(gender_events$event_id)
-	app_events = app_events[event_id %in% ids]
-	gender_app_evets = merge(x = gender_events, y = app_events, by = "event_id", all.x = TRUE)
-	print("gender_app_evets created")
-	rm(ids)
-	rm(app_events)
-	rm(gender_events)
-	gc()
-	
-	
-	# ids = unique(gender_app_evets$app_id)
-	# app_labels = fread("data/app_labels.csv")
-	# app_labels = app_labels[app_id %in% ids]
-	# label_categories = fread("data/label_categories.csv")
-	# app_labels_categories = merge(app_labels, label_categories, by = "label_id", all.x = TRUE)
-	# app_labels_categories$label_id = NULL
-	# print("app_labels_categories created")
-	# rm(app_labels)
-	# rm(ids)
-	# rm(label_categories)
-	# gc()
-	# 
-	# labels = unique(app_labels_categories$category)
-	# app_categories = data.table(app_id = unique(app_labels_categories$app_id))
-	# for(label in labels){
-	# 	app_categories[[label]] = ifelse(app_categories$app_id %in% app_labels_categories$app_id[app_labels_categories$category == label], 1, 0)
-	# }
-	# print("app_categories created")
-	# rm(app_labels_categories)
-	# gc()
-	# 
-	# df = merge(x = gender_app_evets, y = app_categories, by = "app_id", all.x =  TRUE)
-	# print("df created")
-	# rm(app_categories)
-	# rm(gender_app_evets)
-	# gc()
-	# 
-	return(gender_app_evets)
 }
 
 load_app_labels <- function (){
@@ -100,49 +34,17 @@ load_app_labels <- function (){
 	return(app_categories)
 }
 
-merge_data <- function(gender_age, phone_brand_device_model, app_labels, label_categories, app_events, events){
-	app_labels_categories = merge(app_labels, label_categories, by = "label_id", all.x = TRUE)
-	app_labels_categories$label_id = NULL
-	print("app_labels_categories created")
-	rm(app_labels)
-	rm(label_categories)
-	gc()
-	
-	labels = unique(app_labels_categories$category)
-	app_categories = data.table(app_id = unique(app_labels_categories$app_id))
-	for(label in labels){
-		app_categories[[label]] = ifelse(app_categories$app_id %in% app_labels_categories$app_id[app_labels_categories$category == label], 1, 0)
-	}
-	print("app_categories created")
-	rm(app_labels_categories)
-	gc()
-	
-	
-	app_events_categories = merge(app_events, app_categories, by = "app_id", all.x = TRUE)
-	print("app_events_categories created")
-	rm(app_events)
-	rm(app_categories)
-	gc()
-	
-	gender_age_devices = merge(x = gender_age, y = phone_brand_device_model, 
-				by = "device_id", all.x = TRUE)
-	print("gender_age_devices created")
-	rm(gender_age)
-	rm(phone_brand_device_model)
-	gc()
-	
-	events_app = merge(x =app_events_categories, y = events, by = "event_id", all.y = TRUE)
-	print("events_app created")
-	rm(app_events_categories)
-	rm(events)
-	gc()
-	
-	gender_events = merge(gender_age_devices, events_app, by = "device_id", all.x = TRUE)
-	print("gender_events created")
-	rm(gender_age_devices)
-	rm(events_app)
-	gc()
-	
-	
-	return(gender_events)
+get_geo_statistics <- function(events){
+	sum = events[, .(min_lon = min(longitude, na.rm = TRUE), 
+					 max_lon = max(longitude, na.rm = TRUE), 
+					 mean_lon =  mean(longitude, na.rm = TRUE), 
+					 sd_lon = sd(longitude, na.rm = TRUE), 
+					 median_lon = median(longitude, na.rm = TRUE), 
+					 min_lat = min(latitude, na.rm = TRUE), 
+					 max_lat = max(latitude, na.rm = TRUE), 
+					 mean_lat =  mean(latitude, na.rm = TRUE), 
+					 sd_lat = sd(latitude, na.rm = TRUE),
+					 median_lat = median(latitude, na.rm = TRUE)), 
+				 by = device_id]
+	return(sum)
 }
--- src/script.R
+++ src/script.R
@@ -3,18 +3,37 @@ library(bit64)
 library(caret)
 source("src/preprocessing_functions.R")
 source("src/classification.R")
-
+library(rworldmap)
 
 phone_brand_device_model = fread("data/English_phone_brand_device_model.csv")
 phone_brand_device_model = phone_brand_device_model[duplicated(phone_brand_device_model, by = "device_id") == FALSE]
 phone_brand_device_model$phone_brand = as.factor(phone_brand_device_model$phone_brand)
 phone_brand_device_model$device_model = as.factor(phone_brand_device_model$device_model)
+phone_brand_device_model$device_id = as.character(phone_brand_device_model$device_id)
 
+events =  fread("data/events.csv")
+events$device_id = as.character(events$device_id)
+events = split_timestamp(events)
+events[(latitude < 10) & (latitude > -10) & (longitude > -10) & (longitude < 10), longitude := NA]
+events[(latitude < 10) & (latitude > -10), latitude := NA]
+
+events_count_table = table(events$device_id)
+events_count_table = as.data.table(events_count_table)
+colnames(events_count_table) = c("device_id", "count")
 
 gender_age_train = fread("data/gender_age_train.csv")
-gender_age_devices_train = merge(x = gender_age_train, y = phone_brand_device_model,
-								 by = "device_id", all.x = TRUE)
-rownames(gender_age_devices_train) = gender_age_devices_train$device_id
+gender_age_train$device_id = as.character(gender_age_train$device_id)
+gender_age_train = merge(x = gender_age_train, y = phone_brand_device_model,
+						 by = "device_id", all.x = TRUE)
+rownames(gender_age_train) = gender_age_train$device_id
+
+gender_age_train = merge(gender_age_train, events_count_table, 
+						 by = "device_id", all.x = TRUE)
+geo_statistics = get_geo_statistics(events)
+gender_age_train = merge(gender_age_train, geo_statistics, by = "device_id", 
+						 all.x = TRUE)
+# gender_age_train = 
+
 
 gender_age_test =  fread("data/gender_age_test.csv")
 gender_age_devices_test = merge(x = gender_age_test, y = phone_brand_device_model, 
@@ -27,33 +46,3 @@ train_set = train[data_partition]
 test_set = train[-data_partition]
 
 mll = grid_search(train_set, test_set)
-
-# test = create_set(gender_age_devices_test)
-# 
-# model = create_model(train)
-# 
-# predictions = predict_model(model, test)
-
-# app_events = fread("data/app_events.csv")
-# app_labels = fread("data/app_labels.csv")
-# events = fread("data/events.csv")
-# gender_age_test = fread("data/gender_age_test.csv")
-# gender_age_train = fread("data/gender_age_train.csv")
-# label_categories = fread("data/label_categories.csv")
-# phone_brand_device_model = fread("data/phone_brand_device_model.csv")
-# sample_submission = fread("data/sample_submission.csv")
-
-# ids_train = unique(gender_age_train$device_id)
-# ids_test = unique(gender_age_test$device_id)
-
-
-# events_train = events[device_id %in% ids_train]
-# events_test = events[device_id %in% ids_test]
-# rm(events)
-# gc()
-# 
-# phone_brand_device_model = phone_brand_device_model[duplicated(phone_brand_device_model, by = NULL) == FALSE]
-# app_labels = app_labels[duplicated(app_labels, by = NULL) == FALSE]
-# gc()
-# 
-# train = merge_data(gender_age_train, phone_brand_device_model, app_labels, label_categories, app_events, events_train)
